688b0d47e03f380d268239086260499a863bda05
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-19 14:40:30 -0700

    add more javascript and jquery logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
-  $("#trackSuggester").submit(function(event) {
-    event.preventDefault();
+  $(".trackSuggester").submit(function(event) {
+  event.preventDefault();
 
     var name = $("input#nameInput").val();
 
-    var value1 = parseInt($("input:radio[name=question1]:checked").val());
-    var value2 = parseInt($("input:radio[name=question2]:checked").val()); //do not included in "sum"
-    var value3 = parseInt($("input:radio[name=question3]:checked").val());
-    var value4 = parseInt($("input:radio[name=question4]:checked").val());
-    var value5 = parseInt($("input:radio[name=question5]:checked").val());
-    var sum = (value1 + value3 + value4 + value5);
+    var q1 = parseInt($("input:radio[name=question1]:checked").val());
+    var q2 = $("input:radio[name=question2]:checked").val(); //do not included in "sum"
+    var q3 = parseInt($("input:radio[name=question3]:checked").val());
+    var q4 = parseInt($("input:radio[name=question4]:checked").val());
+    var q5 = parseInt($("input:radio[name=question5]:checked").val());
+    var sum = (q1 + q3 + q4 + q5);
 
 
-    // if ("1" > "0") {
-      $("p").css({'color': 'red'});
-      // $("#css").show();
-    // }  else if ($("input#question3").val() === "android" || $("input#question5").val() === "android") {
-    //   $("#android").show();
-    // } else if (sum <= 6) {
-    //   $("#c").show();
-    // } else if (sum > 6 && sum <= 9) {
-    //   $("#php").show();
-    // } else if (sum > 9) {
-    //   $("#ruby").show();
+    if (q2 === "css") {
+      $("#android, #c, #php, #ruby").hide();
+      $("#css, #suggest").show();
+    }
+    else if (q3 === 0 && q4 === 0) {
+      $("#css, #c, #php, #ruby").hide();
+      $("#android, #suggest").show();
+    }
+    else if (sum <= 6) {
+      $("#css, #android, #php, #ruby").hide();
+      $("#c, #suggest").show();
+    }
+    else if (sum > 6 && sum <= 9) {
+      $("#css, #android, #c, #ruby").hide();
+      $("#php, #suggest").show();
+    }
+    else if (sum > 9) {
+      $("#css, #android, #c, #php").hide();
+      $("#ruby, #suggest").show();
+    }
+
   });
 
 });
