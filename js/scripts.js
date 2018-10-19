820b8fe65c049bfa8940a5b0d9eb8f0e99445486
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-19 12:22:06 -0700

    add remaining questions and start of js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
-  $(".formOne").submit(function(event) {
+  $("#formSuggester").submit(function(event) {
   event.preventDefault();
 
-  var operator1 = parseInt($("input:radio[name=question1]:checked").val());
-  var operator2 = parseInt($("input:radio[name=question2]:checked").val());
-  var operator3 = parseInt($("input:radio[name=question3]:checked").val());
+  var value1 = parseInt($("input:radio[name=question1]:checked").val());
+  var value3 = parseInt($("input:radio[name=question3]:checked").val());
 
-  var sum = (operator1 + operator2 + operator3);
-
-  }
-
+  var sum = (value1 + value3);
 
 });
 
