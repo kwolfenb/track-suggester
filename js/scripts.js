dc3d81a07c1c643c97e59f62138d552dd769a104
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-19 17:16:56 -0700

    Fix a typo in the track names

--- js/scripts.js
+++ js/scripts.js
@@ -19,27 +19,27 @@ $(document).ready(function() {
     if (q2 === "css") {
       $("#android, #c, #php, #ruby").hide();
       $("#suggest #css").show();
-      $(".track").text("CSS / Design");
+      $(".track").text("CSS/Design");
     }
     else if (q3 === 0 && q4 === 0) {
       $("#css, #c, #php, #ruby").hide();
       $("#android, #suggest").show();
-      $(".track").text("Java / Android");
+      $(".track").text("Java/Android");
     }
     else if (sum <= 6) {
       $("#css, #android, #php, #ruby").hide();
       $("#c, #suggest").show();
-      $(".track").text("C# / .NET");
+      $(".track").text("C#/.NET");
     }
     else if (sum > 6 && sum <= 9) {
       $("#css, #android, #c, #ruby").hide();
       $("#php, #suggest").show();
-      $(".track").text("PHP / Drupal");
+      $(".track").text("PHP/Drupal");
     }
     else if (sum > 9) {
       $("#css, #android, #c, #php").hide();
       $("#ruby, #suggest").show();
-      $(".track").text("Ruby / Drupal");
+      $(".track").text("Ruby/Rails");
     }
 
   });
